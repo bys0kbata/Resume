0e24367eb2245beba8a643367d90ede0371c26dc
bys0kbata <92207641+bys0kbata@users.noreply.github.com>
2024-05-19 01:31:42 +0400

    Create App

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {Avatar} from "@mui/material";
+import {useArrayState} from "rooks";
+
+function App() {
+
+  return (
+    <>
+        <Avatar
+            alt="Remy Sharp"
+            src="/src/assets/images.jpg"
+            sx={{ width: 56, height: 56 }}
+
+        />
+    </>
+  )
+}
+
+export default App
